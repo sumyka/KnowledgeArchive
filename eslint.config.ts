a7f2a248fbd6b3f8f9c93f5806c115dc29f4ae47
sumyka <206566026+sumyka@users.noreply.github.com>
2025-12-04 22:07:32 +0900

    fix: eslintとprettierの不整合を修正

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,4 @@
 import tseslint from 'typescript-eslint';
-import { fixupConfigRules } from '@eslint/compat';
 import eslint from '@eslint/js';
 import globals from 'globals';
 import nextPlugin from '@next/eslint-plugin-next';
@@ -9,30 +8,17 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
 	eslint.configs.recommended,
-	...tseslint.configs.recommended,
+	tseslint.configs.recommended,
+
+	// React Hooks（Flat Config 対応）
+	reactHooksPlugin.configs.flat.recommended,
 
 	// Next.js 用ルール
 	{
 		files: ['**/*.{js,jsx,ts,tsx}'],
 		plugins: {
 			'@next/next': nextPlugin,
-		},
-		rules: {
-			// '@next/next/no-duplicate-scoped-css': 'warn', <- 誤検知が多いため無効化
-			'@next/next/no-html-link-for-pages': [
-				'error',
-				'src/client/src/app',
-			],
-			'@next/next/no-img-element': 'warn',
-		},
-	},
-
-	// React 用ルール
-	fixupConfigRules({
-		files: ['**/*.{js,jsx,ts,tsx}'],
-		plugins: {
 			react: reactPlugin,
-			'react-hooks': reactHooksPlugin as any,
 		},
 		settings: {
 			react: {
@@ -40,22 +26,26 @@ export default tseslint.config(
 			},
 		},
 		rules: {
+			// '@next/next/no-duplicate-scoped-css': 'warn', <- エラーが発生するので一時的に無効化
+			'@next/next/no-html-link-for-pages': [
+				'error',
+				'src/client/src/app',
+			],
+			'@next/next/no-img-element': 'warn',
+
+			// React
 			'react/react-in-jsx-scope': 'off',
 			'react/jsx-uses-react': 'off',
 			'react/prop-types': 'off',
 			'react/self-closing-comp': 'error',
-
-			'react-hooks/rules-of-hooks': 'error',
-			'react-hooks/exhaustive-deps': 'warn',
-
 			'react/jsx-filename-extension': [
 				'error',
 				{ extensions: ['.jsx', '.tsx'] },
 			],
 		},
-	}),
+	},
 
-	// グローバル変数設定
+	// グローバル変数
 	{
 		languageOptions: {
 			globals: {
@@ -65,5 +55,6 @@ export default tseslint.config(
 		},
 	},
 
+	// Prettier
 	prettierConfig
 );
