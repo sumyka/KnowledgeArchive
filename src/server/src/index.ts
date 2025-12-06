bd013ee5fe398b5fa809d44412932c2fa49afe91
sumyka <206566026+sumyka@users.noreply.github.com>
2025-12-07 02:37:12 +0900

    lint: リンター、フォーマッターを適用

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -6,10 +6,12 @@ const port = 4000;
 const HOST = '0.0.0.0';
 
 // フロントからのアクセス許可
-app.use(cors({
-	origin: 'http://localhost:3000',
-	credentials: true,
-}));
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+	})
+);
 
 app.use(express.json());
 
@@ -21,14 +23,14 @@ app.get('/', (req: Request, res: Response) => {
 // 書籍追加エンドポイント
 app.post('/api/books', (req: Request, res: Response) => {
 	const { isbn } = req.body;
-	
+
 	console.log(`[Server] 受け取ったISBN: ${isbn}`);
 
 	// TODO: ISBNを使って書籍情報を取得する。<- OpenBD予定
-	res.status(200).json({ 
-		success: true, 
+	res.status(200).json({
+		success: true,
 		message: `ISBN ${isbn} を受け付けました`,
-		isbn 
+		isbn,
 	});
 });
 
